de831a968355aa08c9bf100b158610ada334c6a9
Yam Peled <yampeled1@gmail.com>
2018-11-12 20:44:55 +0200

    Added basic test file

---
+++ tests/app_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class MyTestClass(unittest.TestCase):
+
+  def test_equal_numbers(self):
+    self.assertEqual(2, 2)
+
+# runs the unit tests in the module
+if __name__ == '__main__':
+  unittest.main()
